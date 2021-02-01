362856778f56c57bb8da60e810b80ec5aa6a0a28
Matthew <matthewcrews@gmail.com>
2021-02-01 08:28:54 -0800

    starting discrete event ideas

--- ComputationExpression/CETest.fsx
+++ ComputationExpression/CETest.fsx
@@ -1,20 +1,34 @@
+type Chicken = Chicken of string
+    with
+        static member ( ~requires) (a: float, b: float) =
+            a + b
+
+let (.requires) (a: float, b: float) =
+    a + b
+
+
 type BuilderTest () =
+
     member this.Yield (evalResult: bool) =
         evalResult
 
-    member this.For(source: seq<'a>, body:'a -> seq<'b * bool>) =
-        source
-        |> Seq.collect (fun x -> body x |> Seq.map (fun (idx, expr) -> (x, idx), expr))
+    member this.Zero () =
+        false
 
-    member this.For(source: seq<'a>, body:'a -> bool) =
-        source |> Seq.map (fun x -> x, body x)
-
-    member this.Run(source: seq<'a * bool>) =
-        source |> Seq.map (fun (n, c) -> n, c)
-
-    [<CustomOperation("requires")>]
-    member this.Requires(a: bool, b: bool) =
+    [<CustomOperation("requires", MaintainsVariableSpace=true, AllowIntoPattern=true)>]
+    member this.Requires (a: bool, b: bool) =
         a && b
+
+    // member this.For(source: seq<'a>, body:'a -> seq<'b * bool>) =
+    //     source
+    //     |> Seq.collect (fun x -> body x |> Seq.map (fun (idx, expr) -> (x, idx), expr))
+
+    // member this.For(source: seq<'a>, body:'a -> bool) =
+    //     source |> Seq.map (fun x -> x, body x)
+
+    // member this.Run(source: seq<'a * bool>) =
+    //     source |> Seq.map (fun (n, c) -> n, c)
+
 
 let values = [1..10]
 
@@ -22,7 +36,6 @@ let builderTest = BuilderTest ()
 
 let test =
     builderTest {
-        for x in values ->
-            let z = x > 2
-            z requires true
+        let z = false
+        requires (z = true)
     }
