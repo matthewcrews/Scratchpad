d67497db046d70a63db7e08461a3c3dd133c3934
Matthew <matthewcrews@gmail.com>
2021-03-17 22:05:48 -0700

    Cleaning

--- ComputationExpression/TomasAnswer.fsx
+++ ComputationExpression/TomasAnswer.fsx
@@ -16,37 +16,36 @@ let getFood () =
   let randomFood = 
     if rng.NextDouble() > 0.5 then Food.Chicken
     else Food.Rice
-  Plan([GetFood randomFood], randomFood)
+  Plan ([GetFood randomFood], randomFood)
 
 type PlanBuilder () =
 
     member this.For (Plan(steps1, res):Plan<'T>, f:'T -> Plan<'R>) : Plan<'R> =
       printfn "For"
       let (Plan(steps2, res2)) = f res
-    //   Plan(steps1 @ steps2, res2)
-      Plan(steps2 @ steps1, res2)
+      Plan (steps2 @ steps1, res2)
 
     member this.Bind (Plan(steps1, res):Plan<'T>, f:'T -> Plan<'R>) : Plan<'R> =
         printfn "Bind"
         let (Plan(steps2, res2)) = f res
-        // Plan(steps1 @ steps2, res2)
-        Plan(steps2 @ steps1, res2)
+        Plan (steps2 @ steps1, res2)
 
-    // member this.Zero _ = Plan([], ())
     member this.Yield x = 
         printfn "Yield"
-        Plan([], x)
+        Plan ([], x)
+
     member this.Return x = 
         printfn "Retrun"
-        Plan([], x)
+        Plan ([], x)
+
     member this.Run (Plan(p,r)) = 
         printfn "Run"
-        Plan(List.rev p, r)
+        Plan (List.rev p, r)
 
     [<CustomOperation("eat", MaintainsVariableSpace=true)>]
     member this.Eat (Plan(p, r), [<ProjectionParameter>] food) =
         printfn $"Eat: {food}"
-        Plan((Eat (food r))::p, r)
+        Plan ((Eat (food r))::p, r)
 
     [<CustomOperation("sleep", MaintainsVariableSpace=true)>]
     member this.Sleep (Plan(p, r), [<ProjectionParameter>] duration) =
@@ -70,8 +69,3 @@ let (Plan (testPlan, _)) =
   }
 
 testPlan
-
-// let otherPlan =
-//     plan {
-
-//     }
