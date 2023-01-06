b89d6ef86da91c9a6ea881f47c71ab79190bfbac
Matthew Crews <matthew@crews.email>
2023-01-05 21:06:11 -0800

    added

--- FSharpIntro/StructWalkthrough.fsx
+++ FSharpIntro/StructWalkthrough.fsx
@@ -6,26 +6,36 @@ type Chicken =
     }
 
 let c1 = { Name = "Clucky"; Size = 10.0 }
+let c2 = { c1 with Size = 2.0 }
 
+[<Struct>]
 type Turkey =
+    val Name : string
+    val mutable Size : float
+    new (name, size) =
+        { Name = name; Size = size}
+    new name =
+        { Name = name; Size = 1.0 }
+
+let t0 = Turkey ()
+let t1 = Turkey ("Gobble", 20.0)
+
+[<Struct>]
+type Goose (name: string, size: float) =
+    member _.Name = name
+    member _.Size = size
+    new (name: string) =
+        Goose (name, 1.0)
+
+let g1 = Goose ()
+
+type Hawk =
     struct
         val Name : string
         val Size : float
-    new (name, size) =
-        // Validation logic!
-        {
-            Name = name
-            Size = size
-        }
+        new (name, size) =
+            { Name = name; Size = size }
     end
 
-let t = Turkey ("Turkey", 10.0)
-
-type Goose (name: string, size: float) =
-    struct
-        member _.Name = name
-        member _.Size = size
-    end
-
-let g = Goose ("Goosey", 10.0)
-g.Size
+let h0 = Hawk ()
+let h1 = Hawk ("Hawky", 10.0)
