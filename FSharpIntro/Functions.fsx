33b2f5d7cf227f8cb5c60ae986152dd9d260d7cb
Matthew Crews <matthew@crews.email>
2023-01-09 23:08:07 -0800

    added

--- FSharpIntro/Functions.fsx
+++ FSharpIntro/Functions.fsx
@@ -1,3 +1,25 @@
-let myFunc a b = a + b
 
-let myFuncA = myFunc 1
+let myFunction (a: float) (b: float) =
+    a + b
+
+type MyTupleAlias = float * float
+
+let myOtherFunction (a: float, b: float) =
+    a + b
+myOtherFunction (1.0, 2.0)
+
+let myInput = 2.0, 3.0
+
+let anotherFunction (myInput: MyTupleAlias) =
+    1.0
+
+anotherFunction myInput
+
+
+
+
+myFunction 1.0 2.0
+
+let myAdd1 = myFunction 1.0
+myAdd1 10.0
+
